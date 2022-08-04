9df34ee18f2214ae7d0473461200cc28f2f3f167
Fatima <xfatimakx@gmail.com>
2022-08-04 03:04:08 +0000

    Wordsearch exercise

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,24 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    let result = [];
+    for (let i = 0; i < letters[0].length; i ++){
+      result.push([]);
+    }
+  
+    for (let row = 0; row < letters.length; row++) {
+      for (let col = 0; col < letters[row].length; col++) {
+        result[col][row] = letters[row][col];
+      }
+    }
+
+    const verticalJoin = letters.map(ls => ls.join('')) 
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    return false;
+    
 }
 
 module.exports = wordSearch
